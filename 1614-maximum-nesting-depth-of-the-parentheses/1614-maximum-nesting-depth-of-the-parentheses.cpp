09759cfbaf8b210bb3de3a6bcc1a9ae8b8ac068a
Khushi Agarwal <khushiagarwal6350@gmail.com>
2026-07-11 14:01:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (23.9%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int curr=0;
+        int res = 0;
+        for(char &el : s){
+            if(el=='('){
+                curr++;
+                res=max(res,curr);
+            }
+            if(el==')')
+                curr--;
+            }
+        
+        return res;
+    }
+};
