d7ebe96acf016b8568c9ced2aa5456d25736fc31
Khushi Agarwal <khushiagarwal6350@gmail.com>
2026-08-03 18:05:42 +0530

    Time: 0 ms (100%), Space: 23.7 MB (47.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         int el1=INT_MIN;
         int el2=INT_MIN;
         int cnt1=0;
         int cnt2=0;
-        int mini=(n/3)+1;
+        vector<int>ans;
         for(int i=0;i<n;i++){
-            if(cnt1==0&&nums[i]!=el2){
+            if(cnt1==0&&el2!=nums[i]){
                 el1=nums[i];
                 cnt1=1;
             }
-            else if(cnt2==0&&nums[i]!=el1){
-                el2=nums[i];
-                cnt2=1;
-            }
-            else if(el1==nums[i]){
+           else if(cnt2==0&&el1!=nums[i]){
+            el2=nums[i];
+            cnt2=1;
+           }
+           else if(el1==nums[i]){
+            cnt1++;
+           }
+           else if(el2==nums[i]){
+            cnt2++;
+           }
+           else{
+            cnt1--;
+            cnt2--;
+           }
+        }
+        cnt1=0;
+        cnt2=0;
+        int mini = n/3+1;
+        for(int i=0;i<n;i++){
+            if(el1==nums[i]){
                 cnt1++;
             }
             else if(el2==nums[i]){
                 cnt2++;
             }
-            else{
-                cnt1--;
-                cnt2--;
-            }
         }
-        cnt1=0;
-        cnt2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==el1){
-                cnt1++;
-            }
-            else if(nums[i]==el2){
-                cnt2++;
-            }
-        }
-        vector<int>ans;
-            if(cnt1>=mini){
-                ans.push_back(el1);
-            }
-            if(cnt2>=mini){
-                ans.push_back(el2);
-            }
-        
-        return ans;
+
+       if(cnt1>=mini) ans.push_back(el1);
+       if(cnt2>=mini&&el1!=el2) ans.push_back(el2);
+       return ans;
     }
 };
